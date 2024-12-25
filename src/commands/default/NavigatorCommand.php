48ede24387b5b8e8f02b82d48c3c050ec0e9695d
Nassim E <nassim.essid91@gmail.com>
2024-12-25 17:43:54 +0100

    fix 👌

--- src/commands/default/NavigatorCommand.php
+++ src/commands/default/NavigatorCommand.php
@@ -7,13 +7,14 @@ use arkania\commands\CommandBase;
 use arkania\gui\class\DoubleChestMenu;
 use arkania\gui\InventoryContent;
 use arkania\gui\transaction\MenuTransaction;
+use arkania\Main;
 use arkania\session\permissions\DefaultsPermissions;
 use arkania\utils\Utils;
 use pocketmine\command\CommandSender;
 use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\world\sound\DoorSound;
-
 
 class NavigatorCommand extends CommandBase {
 
@@ -31,36 +32,64 @@ class NavigatorCommand extends CommandBase {
     }
 
     protected function onRun(CommandSender $sender, array $parameters) : void {
-
+        if(!$sender instanceof Player) {
+            $sender->sendMessage(Utils::getErrorPrefix() . "Vous devez être un joueur pour exécuter cette commande.");
+            return;
+        }
         $content = [
             new InventoryContent(13, VanillaItems::COMPASS()->setCustomName('§r§fLobby')->setLore(['§r§7Cliquez pour vous téléporter au lobby'])),
             new InventoryContent(30, VanillaItems::COMPASS()->setCustomName('§r§fFaction')->setLore(['§r§7Cliquez pour vous téléporter au Faction (§eDev§7)'])),
             new InventoryContent(32, VanillaItems::COMPASS()->setCustomName('§r§fMinage')->setLore(['§r§7Cliquez pour vous téléporter au Minage (§eDev§7)'])),
         ];
-
-        if(!$sender instanceof Player) {
-            $sender->sendMessage(Utils::getErrorPrefix() . "Vous devez être un joueur pour exécuter cette commande.");
-            return;
-        }
         $menu = new DoubleChestMenu(
             '§8Carte ouverte pour (§9' . $sender->getName() . '§8)',
             true,
             $content,
             function(Player $player, MenuTransaction $transaction) : void {
                 if ($transaction->getSlot() === 13) {
-                    $player->sendMessage(Utils::getPrefix() . "Téléportation vers le lobby...");
-                    $player->broadcastSound(new DoorSound());
-                    $player->transfer('lobby');
+                    if($player->getServer()->getPort() !== 19133) {
+                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le lobby...");
+                        $player->broadcastSound(new DoorSound());
+                        $player->removeCurrentWindow();
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                            function () use ($player) : void {
+                                $player->transfer('lobby');
+                            }), 20);
+                    }else{
+                        $transaction->discard();
+                        $player->removeCurrentWindow();
+                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur du lobby.');
+                    }
                 }
                 if ($transaction->getSlot() === 30) {
-                    $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Faction (§eDev§f)...");
-                    $player->broadcastSound(new DoorSound());
-                    $player->transfer('factiondev');
+                    if($player->getServer()->getPort() !== 19134) {
+                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Faction (§eDev§f)...");
+                        $player->broadcastSound(new DoorSound());
+                        $player->removeCurrentWindow();
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                            function () use ($player) : void {
+                                $player->transfer('factiondev');
+                            }), 20);
+                    }else{
+                        $transaction->discard();
+                        $player->removeCurrentWindow();
+                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de faction.');
+                    }
                 }
                 if ($transaction->getSlot() === 32) {
-                    $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Minage (§eDev§f)...");
-                    $player->broadcastSound(new DoorSound());
-                    $player->transfer('minagedev');
+                    if($player->getServer()->getPort() !== 19135) {
+                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Minage (§eDev§f)...");
+                        $player->broadcastSound(new DoorSound());
+                        $player->removeCurrentWindow();
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                            function () use ($player) : void {
+                                $player->transfer('minagedev');
+                            }), 20);
+                    }else{
+                        $transaction->discard();
+                        $player->removeCurrentWindow();
+                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de minage.');
+                    }
                 }
             }
         );
@@ -68,4 +97,5 @@ class NavigatorCommand extends CommandBase {
         $menu->send($sender);
 
     }
+
 }
