6b2ba6a153c554a224d9bf0631dcf6af3dada44e
Nassim E <nassim.essid91@gmail.com>
2024-12-28 23:44:05 +0100

    new navigator command 🥖

--- src/commands/default/NavigatorCommand.php
+++ src/commands/default/NavigatorCommand.php
@@ -9,13 +9,16 @@ use arkania\gui\InventoryContent;
 use arkania\gui\transaction\MenuTransaction;
 use arkania\gui\transaction\MenuTransactionResult;
 use arkania\Main;
+use arkania\network\servers\ServersStatus;
 use arkania\session\permissions\DefaultsPermissions;
 use arkania\session\Session;
+use arkania\utils\Date;
 use arkania\utils\Utils;
 use pocketmine\command\CommandSender;
 use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\world\sound\DoorSound;
 
 class NavigatorCommand extends CommandBase {
 
@@ -37,79 +40,121 @@ class NavigatorCommand extends CommandBase {
             $sender->sendMessage(Utils::getErrorPrefix() . "Vous devez être un joueur pour exécuter cette commande.");
             return;
         }
-        $content = [
-            new InventoryContent(13, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Lobby§c§l «')->setLore(
-                [
-                    '§r§e §7joueur(s)',
-                    '§r§e(idk pour le status)'
-                ]
-            )),
-            new InventoryContent(30, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Faction§c§l «')->setLore(
-                [
-                    '§r§e0 §7joueur(s)',
-                    '§r§e(idk pour le status)'
-                ]
-            )),
-            new InventoryContent(32, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Minage§c§l «')->setLore(
-                [
-                    '§r§e 0 §7joueur(s)',
-                    '§r§e(idk pour le status)'
-                ]
-            )),
-        ];
-        $menu = new DoubleChestMenu(
-            '§8Carte du voyageur',
-            true,
-            $content,
-            function(Player $player, MenuTransaction $transaction) : MenuTransactionResult {
-                if ($transaction->getSlot() === 13) {
-                    if($player->getServer()->getPort() !== 19133) {
-                        $player->removeCurrentWindow();
-                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le lobby...");
-                        Session::get($player)->sendSound('portal.travel');
-                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
-                            function () use ($player) : void {
-                                $player->transfer('lobby');
-                            }), 15);
-                    }else{
-                        $player->removeCurrentWindow();
-                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur du lobby.');
-                    }
+        Main::getInstance()->getServersManager()->getServers()->then(function (array $servers) use ($sender) {
+            foreach ($servers as $server) {
+                if($server['name'] === 'Lobby') {
+                    $lobby = $server;
+                }elseif($server['name'] === 'Faction') {
+                    $faction = $server;
+                }elseif($server['name'] === 'Minage') {
+                    $minage = $server;
+                }else{
+                    $sender->sendMessage(Utils::getErrorPrefix() . "Erreur lors de la récupération des serveurs.");
+                    $sender->removeCurrentWindow();
+                    return;
                 }
-                if ($transaction->getSlot() === 30) {
-                    if($player->getServer()->getPort() !== 19134) {
-                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Faction (§eDev§f)...");
-                        Session::get($player)->sendSound('portal.travel');
-                        $player->removeCurrentWindow();
-                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
-                            function () use ($player) : void {
-                                $player->transfer('factiondev');
-                            }), 15);
-                    }else{
-                        $player->removeCurrentWindow();
-                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de faction.');
-                    }
-                }
-                if ($transaction->getSlot() === 32) {
-                    if($player->getServer()->getPort() !== 19135) {
-                        $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Minage (§eDev§f)...");
-                        Session::get($player)->sendSound('portal.travel');
-                        $player->removeCurrentWindow();
-                        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
-                            function () use ($player) : void {
-                                $player->transfer('minagedev');
-                            }), 15);
-                    }else{
-                        $player->removeCurrentWindow();
-                        $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de minage.');
-                    }
-                }
-                return $transaction->discard();
             }
-        );
+            if(!isset($lobby)) {
+                $lobby = [
+                    'status' => serialize(['status' => ServersStatus::OFFLINE, 'time' => '?']),
+                    'players' => 0
+                ];
+            }
+            if(!isset($faction)) {
+                $faction = [
+                    'status' => serialize(['status' => ServersStatus::OFFLINE, 'time' => '?']),
+                    'players' => 0
+                ];
+            }
+            if(!isset($minage)) {
+                $minage = [
+                    'status' => serialize(['status' => ServersStatus::OFFLINE, 'time' => '?']),
+                    'players' => 0
+                ];
+            }
+            $content = [
+                new InventoryContent(13, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Lobby§c§l «')->setLore(
+                    [
+                        '§r§e' . $lobby['players'] . ' §7joueur(s)',
+                        '§r' . $this->getColorStatus(unserialize($lobby['status'])['status'])
+                    ]
+                )),
+                new InventoryContent(30, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Faction§c§l «')->setLore(
+                    [
+                        '§r§e' . $faction['players'] . ' §7joueur(s)',
+                        '§r' . $this->getColorStatus(unserialize($faction['status'])['status'])
+                    ]
+                )),
+                new InventoryContent(32, VanillaItems::COMPASS()->setCustomName('§r§c§l»§r§f Minage§c§l «')->setLore(
+                    [
+                        '§r§e' . $minage['players'] . ' §7joueur(s)',
+                        '§r' . $this->getColorStatus(unserialize($minage['status'])['status'])
+                    ]
+                )),
+            ];
+            $menu = new DoubleChestMenu(
+                '§8Carte du voyageur',
+                true,
+                $content,
+                function(Player $player, MenuTransaction $transaction) : MenuTransactionResult {
+                    if ($transaction->getSlot() === 13) {
+                        if($player->getServer()->getPort() !== 19133) {
+                            $player->removeCurrentWindow();
+                            $player->sendMessage(Utils::getPrefix() . "Téléportation vers le lobby...");
+                            Session::get($player)->sendSound('portal.travel');
+                            Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                                function () use ($player) : void {
+                                    $player->transfer('lobby');
+                                }), 15);
+                        }else{
+                            $player->removeCurrentWindow();
+                            $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur du lobby.');
+                        }
+                    }
+                    if ($transaction->getSlot() === 30) {
+                        if($player->getServer()->getPort() !== 19134) {
+                            $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Faction (§eDev§f)...");
+                            Session::get($player)->sendSound('portal.travel');
+                            $player->removeCurrentWindow();
+                            Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                                function () use ($player) : void {
+                                    $player->transfer('factiondev');
+                                }), 15);
+                        }else{
+                            $player->removeCurrentWindow();
+                            $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de faction.');
+                        }
+                    }
+                    if ($transaction->getSlot() === 32) {
+                        if($player->getServer()->getPort() !== 19135) {
+                            $player->sendMessage(Utils::getPrefix() . "Téléportation vers le Minage (§eDev§f)...");
+                            Session::get($player)->sendSound('portal.travel');
+                            $player->removeCurrentWindow();
+                            Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(
+                                function () use ($player) : void {
+                                    $player->transfer('minagedev');
+                                }), 15);
+                        }else{
+                            $player->removeCurrentWindow();
+                            $player->sendMessage(Utils::getErrorPrefix() . 'Vous êtes déjà connecté au serveur de minage.');
+                        }
+                    }
+                    return $transaction->discard();
+                }
+            );
 
-        $menu->send($sender);
+            $menu->send($sender);
+        });
+    }
 
+    protected function getColorStatus(string $status) : string {
+        if($status === ServersStatus::ONLINE) {
+            return '§aEn ligne';
+        }elseif($status === ServersStatus::MAINTENANCE) {
+            return '§6Maintenance';
+        }else{
+            return '§cHors ligne';
+        }
     }
 
 }
